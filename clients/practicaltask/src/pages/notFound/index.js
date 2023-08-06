ea8661655393ea46a8a6e168118a0833c9c51804
Milind-dev <milinddev1011@gmail.com>
2023-08-06 23:40:19 +0530

    fs

--- clients/practicaltask/src/pages/notFound/index.js
+++ clients/practicaltask/src/pages/notFound/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import Jobd from "../jobdatas/Jobdata";
-// import Activejobdata from "../Jobdatas/Activejobdata";
+// import Jobd from "../jobdatas/Jobdata";
+import Activejobdata from "../Jobdatas/Activejobdata";
 export default function NotFound() {
   const [dat, setDat] = useState([]);
   useEffect(() => {
@@ -18,8 +18,8 @@ export default function NotFound() {
         // console.log("item ", item);
         return (
           <>
-            {/* <Activejobdata activetype = {item.type} jobdatassort = {item.jobData} indexes={indexes} /> */}
-            <Jobd  jobdatas = {item.jobData}   activetype = {item.type}/>
+            <Activejobdata activetype = {item.type} jobdatassort = {item.jobData} indexes={indexes} />
+            {/* <Jobd  jobdatas = {item.jobData}   activetype = {item.type} /> */}
           
           </>
         );
