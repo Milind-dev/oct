3340b2efab5b258a7c3150fea5da1c4a1c3310b6
Milind-dev <milinddev1011@gmail.com>
2023-08-06 23:10:23 +0530

    reverse

--- clients/practicaltask/src/pages/notFound/index.js
+++ clients/practicaltask/src/pages/notFound/index.js
@@ -1,29 +1,9 @@
-import React, { useState ,useEffect } from "react";
-import Jobdata from "../jobdatas/Jobdata";
+import React from 'react';
 
-// import Activejobdata from "../Jobdatas/Activejobdata";
 export default function NotFound() {
-  const [dat, setDat] = useState([]);
-  useEffect(() => {
-	fetch("https://ones-seven.vercel.app/")
-      .then((res) => res.json())
-      .then((responses) =>  setDat([responses.data]))		
-	},[]);
-
-
 	return (
-    // <h2 style={{textAlign: 'center'}}> </h2>
-	<>
-      {dat.map((item,indexes) => {
-        // console.log("item ", item);
-        return (
-          <>
-            {/* <Activejobdata activetype = {item.type} jobdatassort = {item.jobData} indexes={indexes} /> */}
-            <Jobdata  jobdatas = {item.jobData}   activetype = {item.type}/>
-          
-          </>
-        );
-      })}
-    </>
-  );
+		<h2 style={{textAlign: 'center'}}>
+            Page Not Found
+        </h2>
+	);
 }
